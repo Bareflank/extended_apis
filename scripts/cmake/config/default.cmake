68e5d136d5f0c185c7e9c5d4aca74a92b747d78e
Rian Quinn <rianquinn@gmail.com>
2018-06-25 19:54:39 -0600

    Patchs to simplify our extensions
    Signed-off-by: Rian Quinn <rianquinn@gmail.com>

--- scripts/cmake/config/default.cmake
+++ scripts/cmake/config/default.cmake
@@ -16,6 +16,46 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+include(${CMAKE_CURRENT_LIST_DIR}/../macros.cmake)
+
+# ------------------------------------------------------------------------------
+# Source Tree
+# ------------------------------------------------------------------------------
+
+set(EAPIS_SOURCE_ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/../../..
+    CACHE INTERNAL
+    "Source root direfctory"
+)
+
+set(EAPIS_SOURCE_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR}/..
+    CACHE INTERNAL
+    "Cmake directory"
+)
+
+set(EAPIS_SOURCE_CONFIG_DIR ${CMAKE_CURRENT_LIST_DIR}
+    CACHE INTERNAL
+    "Cmake configurations directory"
+)
+
+set(EAPIS_SOURCE_DEPENDS_DIR ${CMAKE_CURRENT_LIST_DIR}/../depends
+    CACHE INTERNAL
+    "Cmake dependencies directory"
+)
+
+set(EAPIS_SOURCE_BFSDK_DIR ${CMAKE_SOURCE_DIR}/../../../bfsdk
+    CACHE INTERNAL
+    "bfsdk source dir"
+)
+
+set(EAPIS_SOURCE_BFVMM_DIR ${CMAKE_SOURCE_DIR}/../../../bfvmm
+    CACHE INTERNAL
+    "bfvmm source dir"
+)
+
+# ------------------------------------------------------------------------------
+# Links
+# ------------------------------------------------------------------------------
+
 set(CAPSTONE_URL "https://github.com/bareflank/capstone/archive/v2.0.zip"
     CACHE INTERNAL FORCE
     "Capstone URL"
