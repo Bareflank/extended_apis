1ff602683b30647f55a9ccfd0153c40972ad413a
Connor Davis <davisc@ainfosec.com>
2018-12-06 13:56:12 -0700

    Add max_rte in IOAPIC version register
    Signed-off-by: Connor Davis <davisc@ainfosec.com>

--- bfvmm/include/hve/arch/intel_x64/ioapic.h
+++ bfvmm/include/hve/arch/intel_x64/ioapic.h
@@ -29,6 +29,9 @@ namespace eapis::intel_x64::ioapic
 //
 // Each IOAPIC register is 32-bits, and each RTE is 64
 //
+// NOTE: the subfields have not been implemented yet
+//
+
 using reg_t = uint32_t;
 using rte_t = uint64_t;
 
@@ -39,18 +42,82 @@ namespace id
 {
     constexpr const auto name = "id";
     constexpr const auto indx = 0;
+    constexpr const auto mask = 0xF000000U;
+    constexpr const auto from = 24;
+    constexpr const auto reset_val = 0;
+
+    inline auto get(reg_t val) noexcept
+    { return get_bits(val, mask) >> from; }
+
+    inline void set(reg_t &reg, reg_t val) noexcept
+    { reg = set_bits(reg, mask, val << from); }
+
+    inline void dump(int lev, reg_t val, std::string *msg = nullptr)
+    { bfdebug_subnhex(lev, name, get(val), msg); }
 }
 
 namespace version
 {
     constexpr const auto name = "version";
     constexpr const auto indx = 1;
+    constexpr const auto reset_val = 0x00170011U;
+
+    namespace version
+    {
+        constexpr const auto name = "version";
+        constexpr const auto mask = 0xFFU;
+        constexpr const auto from = 0;
+
+        inline auto get(reg_t val) noexcept
+        { return get_bits(val, mask) >> from; }
+
+        inline void set(reg_t &reg, reg_t val) noexcept
+        { reg = set_bits(reg, mask, val << from); }
+
+        inline void dump(int lev, reg_t val, std::string *msg = nullptr)
+        { bfdebug_subnhex(lev, name, get(val), msg); }
+    }
+
+    namespace max_rte
+    {
+        constexpr const auto name = "max_rte";
+        constexpr const auto mask = 0xFF0000U;
+        constexpr const auto from = 16;
+
+        inline auto get(reg_t val) noexcept
+        { return get_bits(val, mask) >> from; }
+
+        inline void set(reg_t &reg, reg_t val) noexcept
+        { reg = set_bits(reg, mask, val << from); }
+
+        inline void dump(int lev, reg_t val, std::string *msg = nullptr)
+        { bfdebug_subnhex(lev, name, get(val), msg); }
+    }
+
+    inline void dump(int lev, reg_t val, std::string *msg = nullptr)
+    {
+        version::dump(lev, val, msg);
+        max_rte::dump(lev, val, msg);
+    }
 }
 
 namespace arbid
 {
     constexpr const auto name = "arbid";
     constexpr const auto indx = 2;
+
+    constexpr const auto mask = 0xF000000U;
+    constexpr const auto from = 24;
+    constexpr const auto reset_val = 0;
+
+    inline auto get(reg_t val) noexcept
+    { return get_bits(val, mask) >> from; }
+
+    inline void set(reg_t &reg, reg_t val) noexcept
+    { reg = set_bits(reg, mask, val << from); }
+
+    inline void dump(int lev, reg_t val, std::string *msg = nullptr)
+    { bfdebug_subnhex(lev, name, get(val), msg); }
 }
 
 namespace redtbl
@@ -59,7 +126,6 @@ namespace redtbl
     constexpr const auto indx = 16;
     constexpr const auto size = 24;
 }
-
 }
 
 #endif
